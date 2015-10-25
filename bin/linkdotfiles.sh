204a6a901445f5de1a995981fe7e9168ec80c8c4
root <root@fuel.domain.tld>
2015-10-25 15:11:09 +0000

    Fixes for link creation script

--- bin/linkdotfiles.sh
+++ bin/linkdotfiles.sh
@@ -5,33 +5,31 @@
 ############################
 
 ########## Variables
+GLOBIGNORE="none"
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 #files=".mc .ssh .bashrc .vimrc .vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
 cd $dir
-linkfiles=".* *"
+linkfiles="*"
 cd
 createdirs=".vimbackup .vimswap .vimundo"
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $linkfiles; do
-  echo "Moving any existing dotfiles from ~ to $olddir"
-  [ -e ~/$file ] && mv ~/$file ~/dotfiles_old/
-  echo "Creating symlink to $file in home directory."
-  ln -s $dir/$file ~/$file
+  #echo "Moving any existing dotfiles from ~ to $olddir"
+  [ -e ~/$file ] && [ ! -L ~/$file ] && ( set -x; mv ~/$file ~/dotfiles_old/ )
+  #echo "Creating symlink to $file in home directory."
+  [ ! -e ~/$file ] && ( set -x; ln -s $dir/$file ~/$file; )
+  set +x
 done
 
-for dir in $createdirs; do [ ! -e "$dir" ]  && mkdir ~/$dir; done
+for dir in $createdirs; do [ ! -e ~/"$dir" ]  && mkdir ~/$dir; done
